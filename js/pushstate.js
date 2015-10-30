738b51b3a41fbd1eb5da60ce8ca277cab28fe56f
hcpark <pchuri@gmail.com>
2015-10-30 14:47:18 +0900

    Fix the infinite refresh on the pulls page again.

--- js/pushstate.js
+++ js/pushstate.js
@@ -1,5 +1,5 @@
-if (location.href.split("/").pop() !== "pulls") {
-  $( document ).ajaxComplete(function() {
-    window.postMessage({ action: "lgtm:refresh" }, "*");
+$( document ).ajaxComplete(function() {
+      if (location.href.split("/").pop() !== "pulls") {
+        window.postMessage({ action: "lgtm:refresh" }, "*");
+      }
 });
-}
