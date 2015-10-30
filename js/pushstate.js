fa497381bbf9c5e44b5d6746d1cba2fe328bf045
hcpark <pchuri@gmail.com>
2015-10-30 17:36:55 +0900

    Fix the bug on pulls page.

--- js/pushstate.js
+++ js/pushstate.js
@@ -1,3 +1,7 @@
+$(document).on("pjax:end", function() {
+  window.postMessage({ action: "lgtm:refresh" }, "*");
+});
+
 $( document ).ajaxComplete(function() {
       if (location.href.split("/").pop() !== "pulls") {
         window.postMessage({ action: "lgtm:refresh" }, "*");
