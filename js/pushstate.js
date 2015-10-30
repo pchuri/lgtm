c4b43ccb2cd50262fdf7f1349a83f89fb7733342
hcpark <pchuri@gmail.com>
2015-10-30 20:16:40 +0900

    Fix the infinite refresh.

--- js/pushstate.js
+++ js/pushstate.js
@@ -1,5 +1,6 @@
-$( document ).ajaxComplete(function() {
-      if (location.href.split("/").pop() !== "pulls") {
+$( document ).ajaxComplete(function(e, xhr, settings) {
+      console.log(settings.url);
+      if (settings.url !== "/notifications/header") {
         window.postMessage({ action: "lgtm:refresh" }, "*");
       }
 });
