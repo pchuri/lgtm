28ded305f77c835e12f502b2a846765c994b43ef
hcpark <pchuri@gmail.com>
2015-10-30 17:54:29 +0900

    revert

--- js/pushstate.js
+++ js/pushstate.js
@@ -1,7 +1,3 @@
-$(document).on("pjax:end", function() {
-  window.postMessage({ action: "lgtm:refresh" }, "*");
-});
-
 $( document ).ajaxComplete(function() {
       if (location.href.split("/").pop() !== "pulls") {
         window.postMessage({ action: "lgtm:refresh" }, "*");
